43d2739183d65fc2392a4bd99695caa362bc4fc1
lassenextkbh <lass365f@edu.next.kbh.dk>
2022-10-06 12:43:03 +0200

    fixed favicon error + added actual page navigation

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+var lastPage = "1";
+
 document.querySelectorAll('.tab-bar').forEach(function(item){
 
     let children = Array.from(item.children);
@@ -8,6 +10,10 @@ document.querySelectorAll('.tab-bar').forEach(function(item){
             childItem.addEventListener('click', () => {
                 let color = getComputedStyle(document.documentElement).getPropertyValue(`--theme-${childItem.dataset.theme}`);
                 
+                document.getElementById(lastPage).style.visibility = 'hidden';
+                document.getElementById(childItem.dataset.theme).style.visibility = "visible";
+                lastPage = childItem.dataset.theme;
+
                 circleIndicator.classList.remove('animate');
                 circleIndicator.style.marginLeft = ((25 + childItem.clientWidth) * key) + "px";
                 circleIndicator.classList.add('animate');
